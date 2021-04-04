ebd7857785a5e0c262f8971f5d48942186390b1a
kesiahp18 <kesiahp18@gmail.com>
2021-04-04 14:13:00 -0400

    refactor

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,30 +1,28 @@
 const { User, Thought } = require('../models');
 
 const resolvers = {
-    Query: {
-        thoughts: async (parent, {username}) => {
-            // if username exists set params to an object with username key set to that value but if not return empty object
-            const params = username ? { username } : {};
-            return Thought.find().sort({ createdAt: -1 });
-        },
-        thought: async (parent, { _id }) => {
-            return Thought.findOne({ _id });
-        },
-        //get all users
-        users: async () => {
-            return User.find()
-                .select('-__v -password')
-                .populate('friends')
-                .populate('thoughts');
-        },
-        //get a user by username
-        user: async (parent, { username }) => {
-            return User.findOne({ username })
-                .select('-__v -password')
-                .populate('friends')
-                .populate('thoughts');
-        }
+  Query: {
+    users: async () => {
+      return User.find()
+        .select('-__v -password')
+        .populate('thoughts')
+        .populate('friends');
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts');
+    },
+    thoughts: async (parent, { username }) => {
+      // if username exists set params to an object with username key set to that value but if not return empty object
+      const params = username ? { username } : {};
+      return Thought.find(params).sort({ createdAt: -1 });
+    },
+    thought: async (parent, { _id }) => {
+      return Thought.findOne({ _id });
     }
-}
+  }
+};
 
 module.exports = resolvers;
